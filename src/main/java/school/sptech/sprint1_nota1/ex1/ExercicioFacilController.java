2711019cb03ad353bae2f50f61893b248a394fad
lucaspaessptech <lucas.paes@sptech.school>
2025-08-20 14:09:56 -0300

    Exercicios realizados.

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,20 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+        for (int i = 0; i < palavra.length()/2; i++){
+            palavra = palavra.toLowerCase();
+            if(palavra.charAt(i) != palavra.charAt(palavra.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -4,11 +4,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+        if(n <= 0){
+            ExercicioDificilResponse resposta = new ExercicioDificilResponse(0, 0);
+            return resposta;
+        }
+        if(n == 1){
+            ExercicioDificilResponse resposta = new ExercicioDificilResponse(1, 1);
+            return resposta;
+
+        }
+        List<Integer> sequencia = new ArrayList<>();
+        sequencia.add(0);
+        sequencia.add(1);
+        int soma = 1;
+        int termoAtual= 0;
+        for (int i = 2; i <= n; i++){
+            sequencia.add(sequencia.get(i-2)+sequencia.get(i-1));
+            termoAtual = sequencia.get(i);
+            soma+=termoAtual;
+        }
+        ExercicioDificilResponse resposta = new ExercicioDificilResponse(termoAtual, soma);
+        return resposta;
     }
 }
